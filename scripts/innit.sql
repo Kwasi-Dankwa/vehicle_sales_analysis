70585c171dd161269cab7288ddf2b71cf30973e7
Kwasi Dankwa <104780360+Kwasi-Dankwa@users.noreply.github.com>
2025-06-24 03:15:12 -0400

    Create innit.sql

---
+++ scripts/innit.sql
@@ -0,0 +1,19 @@
+-- Creating Car Info Database --
+USE master;
+GO 
+
+CREATE DATABASE CARDB;
+GO 
+
+USE CARDB;
+GO 
+
+-- Creating Schema --
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
