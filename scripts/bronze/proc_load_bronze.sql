13cd9ac4740242bfec20731cc828821f1f449251
Kwasi Dankwa <104780360+Kwasi-Dankwa@users.noreply.github.com>
2025-06-29 12:43:28 -0400

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -16,6 +16,23 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.carprices';
+		TRUNCATE TABLE bronze.carprices;
+		PRINT '>> Inserting Data Into: bronze.carprices';
 
 BULK INSERT bronze.carprices
 		FROM 'C:\Users\kdank\Downloads\car_prices.csv\car_prices.csv'
@@ -25,5 +42,21 @@ BULK INSERT bronze.carprices
 			TABLOCK
 );
 
+SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+
 select *
 from bronze.carprices
+
+EXEC bronze.load_bronze -- execute the stored procedure --
