19abfc218ee159197d4bcde4d6b9daeb21cc5f2c
The-Nygma <edankwa14@gmail.com>
2025-07-19 22:37:23 -0400

    fixed errors by removing vin duplicates and fixing sale date format

--- Vehicle_Analysis_wh/models/staging/stg_car_sales.sql
+++ Vehicle_Analysis_wh/models/staging/stg_car_sales.sql
@@ -1,33 +1,70 @@
--- models/staging/stg_car_sales.sql
 {{ config(materialized='view') }}
+
+WITH base_data AS (
+    SELECT
+        yr,
+        UPPER(TRIM(make)) AS make,
+        UPPER(TRIM(model)) AS model,
+        UPPER(TRIM(trm)) AS trm,
+        UPPER(TRIM(body)) AS body,
+        UPPER(TRIM(transmission)) AS transmission,
+        vin,
+        CASE
+            WHEN LENGTH(ste) > 2 THEN NULL  
+            ELSE UPPER(TRIM(ste))
+        END AS ste,
+        condition,
+        odometer,
+        CASE
+            WHEN TRY_TO_NUMBER(color) IS NOT NULL THEN NULL
+            ELSE UPPER(TRIM(color))
+        END AS color,
+        UPPER(TRIM(interior)) AS interior,
+        UPPER(TRIM(seller)) AS seller,
+        mmr,
+        sellingprice,
+
+        -- Step 1: Extracted the first 15 characters: "Tue Dec 16 2014"
+        -- Step 2: Converted to DATE
+        TRY_TO_DATE(SUBSTR(saledate, 1, 15), 'DY MON DD YYYY') AS saledate_date
+
+    FROM {{ source('cardb', 'car_sales') }}
+    WHERE vin IS NOT NULL AND make IS NOT NULL
+),
+
+-- retains unique vin by keeping the most recent saledate--
+deduped_data AS (
+    SELECT *
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY vin ORDER BY saledate_date DESC) AS row_num
+        FROM base_data
+    )
+    WHERE row_num = 1
+)
 
 SELECT
     yr,
-    UPPER(TRIM(make)) AS make,
-    UPPER(TRIM(model)) AS model,
-    UPPER(TRIM(trm)) AS trm,
-    UPPER(TRIM(body)) AS body,
-    UPPER(TRIM(transmission)) AS transmission,
+    make,
+    model,
+    trm,
+    body,
+    transmission,
     vin,
-    CASE
-        WHEN LENGTH(ste) > 2 THEN NULL  
-        ELSE UPPER(ste)
-    END AS ste,
+    ste,
     condition,
     odometer,
-    CASE
-        WHEN TRY_TO_NUMBER(color) IS NOT NULL THEN NULL
-        ELSE color
-    END AS color,
-    UPPER(TRIM(interior)) AS interior,
-    UPPER(TRIM(seller)) AS seller,
+    color,
+    interior,
+    seller,
     mmr,
     sellingprice,
-    CASE
-        WHEN saledate IS NOT NULL AND POSITION(' GMT' IN saledate) > 4 THEN
-            TRY_TO_DATE(SUBSTR(saledate, 5, POSITION(' GMT' IN saledate) - 5))
-        ELSE NULL
-    END AS saledate
-FROM {{ source('cardb', 'car_sales') }}
-WHERE vin IS NOT NULL
-AND make IS NOT NULL
+
+    -- Format as 'YYYY/MM/DD'
+    TO_CHAR(saledate_date, 'YYYY/MM/DD') AS saledate
+
+FROM deduped_data
+
+
+
+
